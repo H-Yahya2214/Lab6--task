c30045d3c56ce18020ee1914e0f6e47dac95128b
Habiba Ragaey <125299441+H-Yahya2214@users.noreply.github.com>
2024-11-14 17:35:41 +0200

    Update Test_Calculator.py

--- Test_Calculator.py
+++ Test_Calculator.py
@@ -1,21 +1,17 @@
 import unittest
 from calculator import Calculator
 
-class TestCalculator(unitest.TestCase):
+class TestCalculator(unittest.TestCase):
 
-  def Test_add(self):
-    
-    result = Calculator.add(4,9)
-    self.assertEqual(result,13)
+    def test_add(self):
+        result = Calculator.add(4, 9)
+        self.assertEqual(result, 13)
 
+        result = Calculator.add(1, 3)
+        self.assertEqual(result, 4)
 
-    result = Calculator.add(1,3)
-    self.assertEqual(result,4)
-
-
-    result = Calculator.add(20,5)
-    self.assertEqual(result,25)
-
+        result = Calculator.add(20, 5)
+        self.assertEqual(result, 25)
 
 if __name__ == '__main__':
-  unittest.main()
+    unittest.main()

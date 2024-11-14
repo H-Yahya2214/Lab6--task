6822688a33df46023640fc9deb9850ab8c500450
Habiba Ragaey <125299441+H-Yahya2214@users.noreply.github.com>
2024-11-14 17:17:35 +0200

    Create Test_Calculator.py

---
+++ Test_Calculator.py
@@ -0,0 +1,21 @@
+import unittest
+from calculator import Calculator
+
+class TestCalculator(unitest.TestCase):
+
+  def Test_add(self):
+    
+    result = Calculator.add(4,9)
+    self.assertEqual(result,13)
+
+
+    result = Calculator.add(1,3)
+    self.assertEqual(result,4)
+
+
+    result = Calculator.add(20,5)
+    self.assertEqual(result,25)
+
+
+if __name__ == '__main__':
+  unittest.main()
